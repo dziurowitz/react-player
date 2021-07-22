373ffc882d7d720e78efd8f95cce7c31b7dda57f
Wojciech Dziurowicz <wdziurowicz@gmail.com>
2021-07-22 14:19:28 +0200

    add next/previous buttons, play and pause buttons are now interchangeable depending on player status

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import "./App.css";
 export function App() {
   const URL = "https://examples.devmastery.pl/songs-api/songs";
   const [songs, setSongs] = useState([]);
+
   useEffect(() => {
     fetch(URL).then((response) => {
       if (response.ok) {
@@ -15,8 +16,10 @@ export function App() {
       }
     });
   }, []);
+
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const currentSong = songs[currentSongIndex];
+
   function handleSelectSong(selectedSong) {
     const audioIndex = songs.findIndex(
       (song) => song.audioUrl === selectedSong.audioUrl
@@ -25,13 +28,30 @@ export function App() {
       setCurrentSongIndex(audioIndex);
     }
   }
+
+  const handlePreviousSong = () => {
+    const previousCurrentSongIndex = currentSongIndex;
+    setCurrentSongIndex(
+      (previousCurrentSongIndex + songs.length - 1) % songs.length
+    );
+  };
+
+  const handleNextSong = () => {
+    const previousCurrentSongIndex = currentSongIndex;
+    setCurrentSongIndex((previousCurrentSongIndex + 1) % songs.length);
+  };
+
   return (
     <div className="App">
       {songs.length === 0 ? (
         "Loading..."
       ) : (
         <>
-          <SongPlayer song={currentSong} />
+          <SongPlayer
+            song={currentSong}
+            onPreviousSong={handlePreviousSong}
+            onNextSong={handleNextSong}
+          />
           <Songs>
             <Heading title="Songs" />
             <ul>
--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,26 +1,58 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Heading } from "./Heading";
 import "./SongPlayer.css";
 import PlayArrowRoundedIcon from "@material-ui/icons/PlayArrow";
 import PauseRoundedIcon from "@material-ui/icons/Pause";
+import SkipNextRoundedIcon from "@material-ui/icons/SkipNextRounded";
+import SkipPreviousRoundedIcon from "@material-ui/icons/SkipPreviousRounded";
 
-export function SongPlayer({ showControls = false, song }) {
+export function SongPlayer({
+  showControls = false,
+  song,
+  onPreviousSong,
+  onNextSong
+}) {
   const audioRef = useRef();
   const { audioUrl, coverUrl } = song;
+
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+
+  const handlePreviousSong = () => {
+    onPreviousSong();
+    setIsAudioPlaying(false);
+  };
+  const handleNextSong = () => {
+    onNextSong();
+    setIsAudioPlaying(false);
+  };
+
   return (
     <section className="SongPlayer">
       <Heading title="Music Player" />
       <img width="250" height="250" src={coverUrl} alt="Song cover" />
-      <audio ref={audioRef} key={audioUrl} controls={showControls}>
+      <audio loop ref={audioRef} key={audioUrl} controls={showControls}>
         <source src={audioUrl} />
       </audio>
       <div className="SongPlayerControls">
-        <PlayArrowRoundedIcon
-          onClick={() => audioRef.current.play()}
-        ></PlayArrowRoundedIcon>
-        <PauseRoundedIcon
-          onClick={() => audioRef.current.pause()}
-        ></PauseRoundedIcon>
+        <SkipPreviousRoundedIcon
+          onClick={handlePreviousSong}
+        ></SkipPreviousRoundedIcon>
+        {isAudioPlaying ? (
+          <PauseRoundedIcon
+            onClick={() => {
+              audioRef.current.pause();
+              setIsAudioPlaying(false);
+            }}
+          ></PauseRoundedIcon>
+        ) : (
+          <PlayArrowRoundedIcon
+            onClick={() => {
+              audioRef.current.play();
+              setIsAudioPlaying(true);
+            }}
+          ></PlayArrowRoundedIcon>
+        )}
+        <SkipNextRoundedIcon onClick={handleNextSong}></SkipNextRoundedIcon>
       </div>
     </section>
   );
